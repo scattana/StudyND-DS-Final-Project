7095fef0438ef58f84ca201aa4ab9257a8d005f3
Seth A Cattanach <scattana@student00.cse.nd.edu>
2018-04-26 22:49:31 -0400

    added std

--- booking.h
+++ booking.h
@@ -1,10 +1,10 @@
 #include <string>
 
 struct Booking{
-	string building;		// building name
-	string location;		// space identifier within building (i.e. room)
-	string f_name;			// first name of the person who made the booking
-	string l_name;			// last name of person who made booking
+	std::string building;	// building name
+	std::string location;	// space identifier within building (i.e. room)
+	std::string f_name;		// first name of the person who made the booking
+	std::string l_name;		// last name of person who made booking
 	size_t num_people;		// number of people who will be using the space
 	size_t s_time;			// start time of booking (0 = now)
 	size_t book_len;		// duration in hours of booking (0-48)
